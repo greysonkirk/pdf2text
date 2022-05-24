8fbcee55582e081b3edabf1bf829592125dd6183
greysonkirk <greysonkirk@yahoo.com>
2022-05-24 08:42:20 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const pdfParse = require("pdf-parse");
+const ReadText = require('text-from-image')
+const app = express();
+
+app.use("/", express.static("public"));
+app.use(fileUpload());
+
+app.post("/extract-text", (req, res) => {
+    if (!req.files && !req.files.pdfFile) {
+        res.status(400);
+        res.end();
+    }
+
+    pdfParse(req.files.pdfFile).then(result => {
+        res.send(result.text);
+    });
+});
+
+ReadText('./roe.pdf').then(text => {
+    console.log(text);
+}).catch(err => {
+    console.log(err);
+})
+app.listen(3000);
+// // Start the API server
+// app.listen(PORT, function () {
+//     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+//   });
